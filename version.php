3c3e812cefa47f7b69aed27b5b7c5aba7db4d7c9
Davor Budimir <davorbudimir9@gmail.com>
2024-01-12 11:28:22 +0100

    css fix, version up

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_showbadges';
-$plugin->version   = 2023030100;
+$plugin->version   = 2023120100;
 $plugin->requires  = 2022041900;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0 (Build: 2023030100)';
+$plugin->release   = '1.0.0 (Build: 2023120100)';
